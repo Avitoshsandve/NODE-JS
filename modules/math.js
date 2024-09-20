8b23555f24f7943cd43b6e40e8ba6903f46977d8
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2024-09-20 18:25:00 +0530

    Update math.js

--- modules/math.js
+++ modules/math.js
@@ -1,1 +1,18 @@
 
+// function add(a,b){
+//     return a+b;
+// }
+// function multiply(a,b){
+//     return a*b;
+// }
+// function square(a){
+//     return a*a;
+// }
+// module.exports={
+//     add,
+//     multiply,
+//     square,
+// };
+exports.add=(a,b)=>a+b;
+exports.multiply=(a,b)=>a*b;
+exports.square=(a)=>a*a;
